2f0af0e1512858cbb520b98c740ca33174e5f114
Adrien Jarthon <jobs@adrienjarthon.com>
2013-02-23 17:42:46 +0100

    refactoring start

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
-sudo apt-get install gxmessage
-sudo dpkg -i build/*.deb
-./build/install-jdk-7
-./build/install-ubuntu-tweak
+# Adding a few repos
+sudo add-apt-repository ppa:ubuntu-tweak-testing/ppa
+
+# Updating sources
+sudo apt-get update
+
+# Installing a few packages
+sudo apt-get install gxmessage ubuntu-tweak
+
 
 cp /usr/share/X11/xorg.conf.d/50-synaptics.conf /tmp/50-synaptics.conf.back
 sudo mkdir -p /etc/X11/xorg.conf.d
